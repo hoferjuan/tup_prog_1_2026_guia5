27db79156cf80bc04f00f67c51bd26b8d24465f7
unknown <juanfrahofer@gmail.com>
2026-05-16 20:04:18 -0300

    subiengo guia5

---
+++ Guia5/Ejercicio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaro e inicializo variables
+            int numFinal, i;
+            double acum;
+            acum = 0;
+            i = 0;
+            //Solicitar y leer número final
+            Console.WriteLine("Ingrese el número limite");
+            numFinal = Convert.ToInt32(Console.ReadLine());
+            
+            //Ciclo para la suma de números
+            for (i = 1; i <= numFinal; i++)
+            {
+                acum = acum + i;
+            }
+            Console.WriteLine($"El resultado es {acum}");
+        }
+    }
+}
