011f5a1ef1baa39ec608bb00acc09f42866acfb0
unknown <juanfrahofer@gmail.com>
2026-05-16 20:39:25 -0300

    Actualizando guia5

--- Guia5/Ejercicio2/Program.cs
+++ Guia5/Ejercicio2/Program.cs
@@ -10,7 +10,36 @@ namespace Ejercicio2
     {
         static void Main(string[] args)
         {
+            // Línea superior
+            for (int x = 5; x <= 75; x++)
+            {
+                Console.SetCursorPosition(x, 2);
+                Console.Write("*");
+            }
 
+            // Línea inferior
+            for (int x = 5; x <= 75; x++)
+            {
+                Console.SetCursorPosition(x, 22);
+                Console.Write("*");
+            }
+
+            // Lado izquierdo
+            for (int y = 2; y <= 22; y++)
+            {
+                Console.SetCursorPosition(5, y);
+                Console.Write("*");
+            }
+
+            // Lado derecho
+            for (int y = 2; y <= 22; y++)
+            {
+                Console.SetCursorPosition(75, y);
+                Console.Write("*");
+            }
+
+            Console.SetCursorPosition(0, 24);
+            Console.ReadKey();
         }
     }
 }
---
+++ Guia5/Ejercicio3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Si declaro la variable de control "i" existe fuera del for
+            int i;
+            double num, acum, prom;
+
+            //inicialización de acumulador
+            acum= 0;
+            
+            //De otra maneta for(int i = 1; i<=10; i++) 
+            //Existe i solo para el for
+            //En este caso i tiene alcanze fuera del for.
+
+            for (i = 1; i<=10; i++)
+            {
+                Console.WriteLine("Ingrese un número");
+                num=Convert.ToDouble(Console.ReadLine());
+
+                //Acumular forma abreviada "Aprendiendo otros modos"
+                acum += num;
+            }
+            prom = acum / 10;
+
+            Console.WriteLine($"El promedio de los 10 números es: {prom}");
+            
+        }
+    }
+}
